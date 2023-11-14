69f0e5d692fa3d05e87724070258897817a93b7a
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-14 17:22:59 +0500

    s

---
+++ JWTtoken/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using JWTtoken.Entities;
+using JWTtoken.Services.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTtoken.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ILogger<UserController> _logger;
+        public UserController(IUserService userService,ILogger<UserController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                IEnumerable<Users> users = await _userService.GetAllUsersAsync();
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest("Users not Found or some Exception");
+            }
+        }
+    }
+}

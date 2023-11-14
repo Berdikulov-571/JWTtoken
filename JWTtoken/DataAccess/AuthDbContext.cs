a88a8bb199029e9ca630c4d77b2948fb5823d629
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-14 17:23:05 +0500

    s

---
+++ JWTtoken/DataAccess/AuthDbContext.cs
@@ -0,0 +1,12 @@
+﻿using JWTtoken.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTtoken.DataAccess
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}

7b42608c096cd0f32ebca7712f68c0aeef9edfe4
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-14 17:23:33 +0500

    s

---
+++ JWTtoken/Services/Users/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using JWTtoken.Entities;
+
+namespace JWTtoken.Services.User
+{
+    public interface IUserService
+    {
+        public ValueTask<IEnumerable<Users>> GetAllUsersAsync();
+    }
+}

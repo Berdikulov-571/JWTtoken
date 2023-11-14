f07c159ad7ea6f0e07bdae3af8e42cf685a06efe
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-14 17:23:15 +0500

    s

---
+++ JWTtoken/Entities/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTtoken.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}

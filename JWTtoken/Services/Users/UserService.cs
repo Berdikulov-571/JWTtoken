fc89255a5968edfec8157bb8c0abaca7757bb183
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-14 17:23:37 +0500

    s

---
+++ JWTtoken/Services/Users/UserService.cs
@@ -0,0 +1,19 @@
+﻿using JWTtoken.DataAccess;
+using JWTtoken.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTtoken.Services.User
+{
+    public class UserService : IUserService
+    {
+        private readonly AuthDbContext _context;
+        public UserService(AuthDbContext context)
+        {
+            _context = context;
+        }
+        public async ValueTask<IEnumerable<Users>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}

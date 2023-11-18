a71e50d064875e1705adaa7aa5c4136cb1e8e5b7
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-18 15:37:36 +0500

    User Model Updated

--- JWTtoken/Entities/Users.cs
+++ JWTtoken/Entities/Users.cs
@@ -1,9 +1,12 @@
-﻿namespace JWTtoken.Entities
+﻿using JWTtoken.Enums;
+
+namespace JWTtoken.Entities
 {
     public class Users
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
+        public Role Role { get; set; }
     }
 }

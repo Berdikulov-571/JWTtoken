2aa6ca12e96f911554e2da75ec742f9a0ec1fdcf
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-18 15:37:55 +0500

    Migrations

--- JWTtoken/Migrations/20231118102312_RoleAdded.Designer.cs
+++ JWTtoken/Migrations/20231118102312_RoleAdded.Designer.cs
@@ -3,6 +3,7 @@ using JWTtoken.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JWTtoken.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231118102312_RoleAdded")]
+    partial class RoleAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

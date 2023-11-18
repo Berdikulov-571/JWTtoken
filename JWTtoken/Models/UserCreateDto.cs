9dffa53706a05e3a748deeca1e041836c5c3338d
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-18 15:38:08 +0500

    UserCreateDto Updated

--- JWTtoken/Models/UserCreateDto.cs
+++ JWTtoken/Models/UserCreateDto.cs
@@ -1,8 +1,11 @@
-﻿namespace JWTtoken.Models
+﻿using JWTtoken.Enums;
+
+namespace JWTtoken.Models
 {
     public class UserCreateDto
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; }
     }
 }

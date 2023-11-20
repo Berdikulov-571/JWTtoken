28586462c77d30d958825e4d8cf79b05dd295f57
berdikulov-571 <mygithub571@gmail.com>
2023-11-20 13:54:15 +0500

    DataBase

---
+++ JwtToken2Project/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JwtToken2Project.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtToken2Project.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}

39d19b4bc0a918a1a5f73e9fe5000e0536f9a388
berdikulov-571 <mygithub571@gmail.com>
2023-11-20 13:54:07 +0500

    Controllers

---
+++ JwtToken2Project/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using JwtToken2Project.Entities;
+using JwtToken2Project.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtToken2Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenRepository _tokenRepository;
+        private readonly IUserRepository _userRepository;
+        public AuthController(ITokenRepository tokenRepository,IUserRepository userRepository)
+        {
+            _tokenRepository = tokenRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> LoginAsync(string userName, string password)
+        {
+            User user = await _userRepository.CheckLogin(userName, password);
+
+            if (user != null)
+            {
+                string token = _tokenRepository.GenerateToken(user.UserName, user.Role);
+                return Ok(token);
+            }
+            return NotFound("User Not Found");
+        }
+    }
+}
---
+++ JwtToken2Project/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using JwtToken2Project.DTOs;
+using JwtToken2Project.Entities;
+using JwtToken2Project.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtToken2Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> PostAsync(UserCreateDto model)
+        {
+            await _userRepository.CreateAsync(model);
+
+            return Ok("Sucesfully");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            IEnumerable<User> users = await _userRepository.GetAllAsync();
+
+            return Ok(users);
+        }
+    }
+}
